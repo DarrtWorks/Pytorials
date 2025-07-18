12671b56d14703a2464ab368327673a0094bb584
DarrtWorks <devworksdd@gmail.com>
2025-07-19 00:51:37 +0530

    Refactor layout of GUI elements to use grid instead of pack for better alignment

--- TKKK.py
+++ TKKK.py
@@ -2,9 +2,12 @@ import tkinter as tk
 from tkinter import *
 root = tk.Tk()
 l1 = Label(root,text= 'Geeks for geeks')
-l1.pack(anchor= CENTER)
+l1.grid(row=0,column=2)
 b1 = Button(root, text='Destroy',width=25,command= root.destroy)
-b1.pack()
-
+b1.grid(row=1,column=2)
+Label(root,text='Your First Name:').grid(row=0)
+Label(root,text='Your Second Name:').grid(row=1)
+e1 = Entry(root).grid(row=0,column=1)
+e2 = Entry(root).grid(row=1,column=1)
 
 root.mainloop()
